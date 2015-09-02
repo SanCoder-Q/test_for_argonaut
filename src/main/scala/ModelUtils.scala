d0835df922d666eadbb4f315a90971e64dcca4b8
sunchen115 <sunchen115work@gmail.com>
2015-09-02 11:14:53 +0800

    remove person decode and encode

--- src/main/scala/ModelUtils.scala
+++ src/main/scala/ModelUtils.scala
@@ -9,38 +9,6 @@ object ModelUtils {
   case class Person2(name: String, age : Age)
   case class Person(name: String, age: Int)
 
-  implicit def PersonEncode: EncodeJson[Person] = {
-    jencode2L((p : Person) => (p.name, p.age))("name","age")
-    //  //val jsonObjectAlternative: Json =
-    //  //jObjectAssocList(List("key1" -> jsonNumber, "key2" -> jsonString))
-
-  }
-
-  implicit def PersonDecode: DecodeJson[Person] = {
-    jdecode2L(Person.apply)("name","age")
-  }
-
-//  implicit def AgeDecode: DecodeJson[Age] = {
-//    DecodeJson(c => for{
-//      cnt <- (c --\ "cnt").as[Int]
-//      unit <- (c --\ "unit").as[String]
-//    } yield Age(cnt, unit))
-//  }
-//
-//  implicit def Person2Decode: DecodeJson[Person2] = {
-//    DecodeJson(c => for{
-//      name <- (c --\ "name").as[String]
-//      age <- (c --\ "age").as[Age]
-//  } yield Person2(name, age))
-//  }
-
-  implicit def AgeDecode: DecodeJson[Age] = {
-    jdecode2L(Age.apply)("cnt","unit")
-  }
-
-  implicit def Person2Decode: DecodeJson[Person2] = {
-    jdecode2L(Person2.apply)("name","age")
-  }
 
   def fromObjectToJson(p: Person) : Json = {
     return p.jencode
